ee5cdde9ea7467523fa8e9d6c97f481db4a263fb
edeced <ed_ced@icloud.com>
2021-02-04 01:55:15 -0600

    adds developer emoji

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 function Header(){
     return (
-        <div className="m-4 p-4 text-4xl font-extrabold">MyJobs</div>
+        <div className="m-4 p-4 text-4xl font-extrabold">👨🏾‍💻 MyJobs</div>
     );
 }
 
